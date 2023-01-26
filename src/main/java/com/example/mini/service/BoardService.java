fc55cb4721eedf2702f73b80a6a8100225b49d6d
Jinhyung Rhee <rheejinhyung@gmail.com>
2023-01-26 20:32:05 +0900

    feat: BoardController sorting 기능 추가

--- src/main/java/com/example/mini/service/BoardService.java
+++ src/main/java/com/example/mini/service/BoardService.java
@@ -10,5 +10,7 @@ public interface BoardService {
     int getBoardCount();
     //List<BoardDto> getBoardList(int limit);
     List<Map<String, Object>> getBoardList(int limit);
-    List<Map<String, Object>> getReversedBoardList(int limit);
+    List<Map<String, Object>> getRecentBoards();
+    List<Map<String, Object>> getPastBoards();
+    List<Map<String, Object>> getFamousBoards();
 }
